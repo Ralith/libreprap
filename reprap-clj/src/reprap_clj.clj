da3f7941f9517ef033bdeb1237506aecdd227d73
Elmom <elmo.mantynen@iki.fi>
2011-02-28 15:35:27 +0200

    Some more declarations into reprap-clj

--- reprap-clj/src/reprap_clj.clj
+++ reprap-clj/src/reprap_clj.clj
@@ -13,9 +13,34 @@
   (:callbacks
    (add-cb [int int] int))
   (:functions
-   (rr_enumerate_ports [] void*)))
+   (rr_enumerate_ports [] void*)
+   (rr_create [enum ;; rr_proto
+	       void* void* ;; onsend & data
+	       void* void* ;; onrecv & data
+	       void* void* ;; onreply & data
+	       void* void* ;; onerr & data
+	       void* void* ;; want_writable & data
+	       size_t] ;; resend_cache_size
+	      void*) ;; rr_dev
+   (rr_open [void* ;; rr_dev
+	     constchar* ;; port
+	     long] ;; speed
+	    int)
+   (rr_reset [void*] int) ;; rr_dev
+   (rr_close [void*] int) ;; rr_dev
+   (rr_free [void*] int) ;; rr_dev
+   (rr_enqueue_c [void* ;; rr_dev
+		  enum ;; rr_prio
+		  void* ;; cbdata
+		  constchar*] ;; block
+		 void)
 
 (loadlib libreprap)
 
+(def rr-proto (zipmap [:simple :fived :tonokip] (range)))
+
 (defn enumerate-ports []
   (.getStringArray (rr_enumerate_ports) 0))
+
+(defn create [proto & TODO]
+  (rr_create (rr-proto proto) 0 0 0 0 0 0 0 0 0 64)
