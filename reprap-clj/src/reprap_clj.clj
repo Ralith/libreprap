ac58e61b196a88f1a527fbcd4ebaca69d7df62c0
Elmom <elmo.mantynen@iki.fi>
2011-02-28 23:37:29 +0200

    some more declarations for reprap-clj

--- reprap-clj/src/reprap_clj.clj
+++ reprap-clj/src/reprap_clj.clj
@@ -11,7 +11,10 @@
   (:unions
    (splitint :theint int :packed packed))
   (:callbacks
-   (add-cb [int int] int))
+   (sendcb [void* void* void* constchar* size_t] void)
+   (recvcb [void* void* constchar* size_t] void)
+   (replycb [void* void* void* float] void)
+   (boolcb [void* void* char] void))
   (:functions
    (rr_enumerate_ports [] void*)
    (rr_create [enum ;; rr_proto
@@ -29,11 +32,12 @@
    (rr_reset [void*] int) ;; rr_dev
    (rr_close [void*] int) ;; rr_dev
    (rr_free [void*] int) ;; rr_dev
-   (rr_enqueue_c [void* ;; rr_dev
-		  enum ;; rr_prio
-		  void* ;; cbdata
-		  constchar*] ;; block
-		 void)
+   (rr_enqueue [void* ;; rr_dev
+		enum ;; rr_prio
+		void* ;; cbdata
+                constchar* ;; block
+                size_t] ;; block size
+	       void)))
 
 (loadlib libreprap)
 
@@ -43,4 +47,5 @@
   (.getStringArray (rr_enumerate_ports) 0))
 
 (defn create [proto & TODO]
-  (rr_create (rr-proto proto) 0 0 0 0 0 0 0 0 0 64)
+  (rr_create (rr-proto proto) 0 0 0 0 0 0 0 0 0 0 64))
+
