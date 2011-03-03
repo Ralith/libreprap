111650ee6951431dfaea4c08639a73473c6b162c
Elmom <elmo.mantynen@iki.fi>
2011-03-03 17:57:36 +0200

    creating, resetting and freeing a device struct works in reprap-clj

--- reprap-clj/src/reprap_clj.clj
+++ reprap-clj/src/reprap_clj.clj
@@ -14,15 +14,16 @@
    (sendcb [void* void* void* constchar* size_t] void)
    (recvcb [void* void* constchar* size_t] void)
    (replycb [void* void* void* float] void)
-   (boolcb [void* void* char] void))
+   (boolcb [void* void* char] void)
+   (errcb [void* void* int constchar* size_t]))
   (:functions
    (rr_enumerate_ports [] void*)
    (rr_create [enum ;; rr_proto
-	       void* void* ;; onsend & data
-	       void* void* ;; onrecv & data
-	       void* void* ;; onreply & data
-	       void* void* ;; onerr & data
-	       void* void* ;; want_writable & data
+	       sendcb void* ;; onsend & data
+	       recvcb void* ;; onrecv & data
+	       replycb void* ;; onreply & data
+	       errcb void* ;; onerr & data
+	       boolcb void* ;; want_writable & data
 	       size_t] ;; resend_cache_size
 	      void*) ;; rr_dev
    (rr_open [void* ;; rr_dev
@@ -46,6 +47,35 @@
 (defn enumerate-ports []
   (.getStringArray (rr_enumerate_ports) 0))
 
-(defn create [proto & TODO]
-  (rr_create (rr-proto proto) 0 0 0 0 0 0 0 0 0 0 64))
+(defn create-dev [& {:keys [proto onsend onsend-d onrecv onrecv-d onreply onreply-d onerr onerr-d
+                        want-writable want-writable-d resend-cache-size]
+                 :or {proto :fived resend-cache-size 64}}]
+  (atom (rr_create (rr-proto proto) onsend onsend-d onrecv onrecv-d onreply onreply-d
+             onerr onerr-d want-writable want-writable-d resend-cache-size)))
 
+(defn free-dev [dev]
+  (locking @dev
+    (swap! dev #(do
+                  (when (not %)
+                    (throw (Exception. "Double free")))
+                  (rr_free %)
+                  nil))))
+
+(defn test-free-locking [dev]
+  (future
+    (locking @dev
+      (swap! dev #(do
+                    (println "sleeping")
+                    (Thread/sleep 5000)
+                    (when (not %)
+                      (throw (Exception. "Double free")))
+                    (rr_free %)
+                    (println "freed")
+                    nil))))
+  (free-dev dev))
+
+(defn reset-dev [dev]
+  (locking @dev
+    (when (not @dev)
+      (throw (Exception. "Null pointer dereference")))
+    (rr_reset @dev)))
